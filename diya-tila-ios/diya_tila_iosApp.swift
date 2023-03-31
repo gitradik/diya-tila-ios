ddcce3a307be9e234fb9d13189f7062c18d1ba00
gitradik <radik.softdev@gmail.com>
2023-03-31 08:13:59 +0200

    first commit

---
+++ diya-tila-ios/diya_tila_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  diya_tila_iosApp.swift
+//  diya-tila-ios
+//
+//  Created by Rodion Malakhov on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct diya_tila_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
